803ef82c397671fa81b8c03c5e35a3d4bbd8c0e8
keerat-k <keeratkhandpur@csus.edu>
2025-10-17 13:29:57 -0700

    header

---
+++ frontend/app/App.jsx
@@ -0,0 +1,16 @@
+// App.jsx
+import React from "react";
+import Header from "./Header";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <main>
+        <p>Welcome to my website!</p>
+      </main>
+    </div>
+  );
+}
+
+export default App;
